0623c48dc15584f6e55a67b2b742c8f75e0646a7
Ramkumar811311 <ramkumar070406@gmail.com>
2026-08-06 18:47:14 +0530

    Time: 80 ms (77.69%), Space: 255 MB (15.55%) - LeetHub

---
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -0,0 +1,52 @@
+class Solution {
+    static int timer = 1;
+
+    public static void DFS(int node, int parent, ArrayList<ArrayList<Integer>> adj, int visited[], int dt[], int low[],
+            List<List<Integer>> bridges) {
+        visited[node] = 1;
+        dt[node] = low[node] = timer;
+        timer++;
+
+        for (int it : adj.get(node)) {
+            if (it == parent) {
+                continue;
+            }
+
+            else if (visited[it] == 0) {
+                DFS(it, node, adj, visited, dt, low, bridges);
+                low[node] = Math.min(low[node], low[it]);
+                if (low[it] > dt[node]) {
+                    bridges.add(Arrays.asList(it, node));
+                }
+            } else {
+                low[node] = Math.min(low[node], low[it]);
+            }
+        }
+    }
+
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        int visited[] = new int[n];
+        int dt[] = new int[n];
+        int low[] = new int[n];
+
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (List<Integer> edge : connections) {
+            int u = edge.get(0);
+            int v = edge.get(1);
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        List<List<Integer>> bridges = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == 0) {
+                DFS(i, -1, adj, visited, dt, low, bridges);
+            }
+        }
+        return bridges;
+    }
+}
