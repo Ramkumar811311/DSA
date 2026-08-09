3f4708eddb9351220bd72c91d06dfdca72baa930
Ramkumar811311 <ramkumar070406@gmail.com>
2026-08-09 17:07:37 +0530

    Time: 1 ms (100%), Space: 48.1 MB (38.66%) - LeetHub

--- 2924-find-champion-ii/2924-find-champion-ii.java
+++ 2924-find-champion-ii/2924-find-champion-ii.java
@@ -1,17 +1,17 @@
 class Solution {
     public int findChampion(int n, int[][] edges) {
-        int team[]=new int[n];
+        int indegree[]=new int[n];
 
         for(int edge[] : edges){
             int u=edge[0];
             int v=edge[1];
-            team[v]++;
+            indegree[v]++;
         }
 
         int champian=-1;
 
         for(int i=0;i<n; i++){
-            if(team[i]==0){
+            if(indegree[i]==0){
                 if(champian!=-1){
                     return -1;
                 }
