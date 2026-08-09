4d295e8790605a3dc420ded14a39fb3540313425
Ramkumar811311 <ramkumar070406@gmail.com>
2026-08-09 16:37:42 +0530

    Time: 0 ms (100%), Space: 47.6 MB (18.57%) - LeetHub

--- 2923-find-champion-i/2923-find-champion-i.java
+++ 2923-find-champion-i/2923-find-champion-i.java
@@ -1,27 +1,11 @@
 class Solution {
     public int findChampion(int[][] grid) {
-         int team[] = new int[grid.length];
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (i == j) {
-                    continue;
-                }
-                if (grid[i][j] == 1) {
-                    team[i]++;
-                }
+        int champian = 0;
+        for (int i = 1; i < grid.length; i++) {
+            if (grid[i][champian] == 1) {
+                champian = i;
             }
         }
-
-        int winner = -1;
-        int maxWin = -1;
-
-        for (int i = 0; i < team.length; i++) {
-
-            if (team[i] > maxWin) {
-                maxWin = team[i];
-                winner = i;
-            }
-        }
-        return winner;
+        return champian;
     }
 }
