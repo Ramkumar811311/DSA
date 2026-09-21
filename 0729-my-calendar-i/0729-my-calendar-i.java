d18837da00900ba233fd2ca1ebd4d485f03b3ab1
Ram Kumar <ramkumar070406@gmail.com>
2026-09-21 16:31:31 +0530

    Time: 22 ms (81%), Space: 47.4 MB (41.63%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.java
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -1,19 +1,21 @@
 class MyCalendar {
-      List<int[]> events;
+    TreeSet<int[]> set;
 
     public MyCalendar() {
-        events=new ArrayList<>();
+        set = new TreeSet<>((a, b) -> Integer.compare(a[0], b[0]));
     }
-    
+
     public boolean book(int startTime, int endTime) {
-        for(int event[]:events){
-            int start=event[0];
-            int end=event[1];
-            if(startTime<end && start<endTime){
-                return false;
-            }
+        int[] next = set.ceiling(new int[] { startTime, endTime });
+        int[] prev = set.lower(new int[] { startTime, endTime });
+
+        if (next != null && endTime > next[0]) {
+            return false;
         }
-        events.add(new int[]{startTime,endTime});
+        if (prev != null && prev[1] > startTime) {
+            return false;
+        }
+        set.add(new int[] { startTime, endTime });
         return true;
     }
 }
